ca2c5a03623e41259174a7ad7b8fa4c15edc2c8f
Thineskumar Thilakanathan <thineskumarthilakanathan@Thineskumars-MacBook-Pro.local>
2019-09-17 23:55:26 +0200

    Refactoring state and props, Added alert component and function

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import Axios from 'axios';
 import Search from './components/users/Search';
+import Alert from './components/layout/Alert';
 
 class App extends Component {
   state = {
     users: [],
-    loading: false
+    loading: false,
+    alert: null
   };
 
   /* async componentDidMount() {
@@ -30,13 +32,29 @@ class App extends Component {
     this.setState({ users: res.data.items, loading: false });
   };
 
+  clearUsers = () => this.setState({ users: [], loading: false });
+
+  setAlert = (message, type) => {
+    this.setState({ alert: { message, type } });
+
+    setTimeout(() => this.setState({ alert: null }), 5000);
+  };
+
   render() {
+    const { users, loading, alert } = this.state;
+
     return (
       <div className='App'>
         <Navbar />
         <div className='container'>
-          <Search searchUsers={this.searchUsers} />
-          <Users loading={this.state.loading} users={this.state.users} />
+          <Alert alert={alert} />
+          <Search
+            searchUsers={this.searchUsers}
+            clearUsers={this.clearUsers}
+            showClear={users.length > 0 ? true : false}
+            setAlert={this.setAlert}
+          />
+          <Users loading={loading} users={users} />
         </div>
       </div>
     );
